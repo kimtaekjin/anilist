6e200cacc93c034d77a88684ca0b9e3346f96250
김택진 <qlcdmlrlf@naver.com>
2026-02-02 22:42:05 +0900

    .

--- prontend/src/Page/MainPage/MainPage.jsx
+++ prontend/src/Page/MainPage/MainPage.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { fetchTrendingAnime, fetchCompletedAnime, fetchOVAAnime } from "../../components/items/AniListQuery.jsx";
+import { fetchTrendingAnime, fetchCompletedAnime, fetchOVAAnime } from "../../components/items/AniListQuery";
 import MainPageCard from "./MainPageCard";
 
 const CACHE_DURATION = 24 * 60 * 60 * 1000;

731d915c7e7adb375f7d30f9b1aae5166d851c9d
김택진 <qlcdmlrlf@naver.com>
2026-02-03 12:39:39 +0900

    cors 변경

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
+
 import service from "./routes/service.js";
 import user from "./routes/user.js";
 import post from "./routes/post.js";
@@ -10,30 +11,42 @@ import post from "./routes/post.js";
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.use(cookieParser());
+const allowedOrigins = [
+  "http://localhost:3001",
+  "http://localhost:3000",
+  "https://anilist-rosy.vercel.app",
+  "https://anilist-qk41.vercel.app",
+];
 
-const isProd = process.env.NODE_ENV === "production";
 app.use(
   cors({
-    origin: isProd ? process.env.CLIENT_URL : process.env.SERVER_URL, // 배포 시 도메인 변경 필요
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+      return callback(null, false);
+    },
     credentials: true,
   }),
 );
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use("/service", service);
 app.use("/user", user);
 app.use("/post", post);
 
+app.get("/healthz", (req, res) => res.send("OK"));
+
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("MongoDB 연결 성공");
-  })
-  .catch((error) => {
-    console.log("MongoDB 연결 실패", error);
-  });
+  .then(() => console.log("MongoDB 연결 성공"))
+  .catch((err) => console.error("MongoDB 연결 실패", err));
 
 app.listen(PORT, () => {
   console.log("Server running on port:", PORT);
